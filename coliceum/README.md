53b2f45b9fc079b7aa91d81814a563d8ede1da51
むずでょ <muzudho1@gmail.com>
2024-08-31 01:23:37 +0900

    pexpect for Windows

--- coliceum/README.md
+++ coliceum/README.md
@@ -4,6 +4,13 @@
 上位ディレクトリーの Python スクリプトから import して使ってください  
 
 
+## 必要なパッケージのインストール
+
+```shell
+pip install pexpect
+```
+
+
 ## 実行
 
 binarsi_analysis リポジトリーの最上位ディレクトリーをカレントディレクトリーにしているとします  
@@ -13,3 +20,8 @@ Input:
 ```shell
 python coliceum.py
 ```
+
+
+# 参考文献
+
+📖 [Windowsでpexpectを利用する](https://qiita.com/shita_fontaine/items/c2ceb1e66450d7e09490)  
--- coliceum/__init__.py
+++ coliceum/__init__.py
@@ -1,29 +1,81 @@
-import pexpect
+import pexpect.popen_spawn as psp
 
 
 class Coliceum():
     """対局コロシアム"""
 
+
+    def __init__(self, child_process):
+        """初期化
+
+        Parameters
+        ----------
+        child_process : object
+            このプロセスからオープンした新しいプロセス。このプロセスの子プロセスとしてぶら下げたもの。
+            このプロセス自体をクローズさせる方法が分からない
+        """
+
+        self._proc = child_process
+
+
+    def sendline(self, message):
+        """子プロセスの標準入力へ文字列を送ります"""
+
+        print(f"[Coliceum > sendline]  {message=}")
+        self._proc.sendline(message)
+
+
+    def readlines(self):
+        # FIXME よく分かってない
+        return self._proc.before.decode("utf8", errors="ignore")
+
+
+    def expect_line(self, message, timeout):
+        """子プロセスが出力すると想定した文字列
+        
+        Windows を想定して、改行コードの '\r\n' を末尾に付ける"""
+
+        print(f"[Coliceum > expect_line]  {message=}")
+        self._proc.expect(f"{message}\r\n", timeout=timeout)
+
+
     @staticmethod
     def start():
-        """TODO 開始"""
+        """TODO 開始
 
         # NOTE Python の subprocess はパイプのことだ。アプリケーションが終了して結果をリターンすることを前提としている
         # NOTE Python の multiprocessing は、外部プロセスを起動するものではない。プロセスを生成することを前提としている
         # NOTE Python の pexpect ライブラリーは、メッセージループで入力を待機するアプリケーションに向いているが、UNIX 系に限られる。公式で Windows には対応していないアナウンスがある
         # NOTE Socket 通信は、サーバー・クライアント型を想定している。標準入出力で通信を行う想定の外部プロセスに対応したものではない
-        # NOTE Elixier を使うしかないのでは？
+        # NOTE Windows で pexpect ライブラリーを使ってみたという記事がある
+        # 📖 [Windowsでpexpectを利用する](https://qiita.com/shita_fontaine/items/c2ceb1e66450d7e09490)
+        """
 
         # binarsi_analisis リポジトリー直下の main.py を起動しています
         # engine は対局エンジンプロセス
-        engine = pexpect.spawn('python main.py')
+        print("[Coliceum > start]  spawn: python main.py")
+        coliceum = Coliceum(
+            child_process=psp.PopenSpawn('python main.py'))
 
-        engine.sendline("usi")
+        coliceum.sendline("usi")
 
-        engine.expect("usiok\n", timeout=None)
+        coliceum.expect_line("id name KifuwarabeBinarsi", timeout=None)
 
-        engine.sendline("isready")
+        coliceum.expect_line("id author Muzudho", timeout=None)
 
-        print(engine.before.decode(encoding='utf-8'))
+        coliceum.expect_line("usiok", timeout=None)
 
-        engine.close()
+        coliceum.sendline("isready")
+
+        coliceum.expect_line("readyok", timeout=None)
+
+        coliceum.sendline("usinewgame")
+
+        print(f"""\
+Read lines
+----------
+{coliceum.readlines()}""")
+
+        # 思考エンジンを終了させる
+        coliceum.sendline("quit")
+
