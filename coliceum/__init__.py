5aa5a41f195fda797c237c01a8a82db976f2efa7
むずでょ <muzudho1@gmail.com>
2024-08-31 00:14:12 +0900

    Elixir を使うしかないのでは？

--- coliceum/__init__.py
+++ coliceum/__init__.py
@@ -11,7 +11,8 @@ class Coliceum():
         # NOTE Python の subprocess はパイプのことだ。アプリケーションが終了して結果をリターンすることを前提としている
         # NOTE Python の multiprocessing は、外部プロセスを起動するものではない。プロセスを生成することを前提としている
         # NOTE Python の pexpect ライブラリーは、メッセージループで入力を待機するアプリケーションに向いているが、UNIX 系に限られる。公式で Windows には対応していないアナウンスがある
-        # NOTE Sock はどうか？
+        # NOTE Socket 通信は、サーバー・クライアント型を想定している。標準入出力で通信を行う想定の外部プロセスに対応したものではない
+        # NOTE Elixier を使うしかないのでは？
 
         # binarsi_analisis リポジトリー直下の main.py を起動しています
         # engine は対局エンジンプロセス
